264c7e90a55314f24fe43fb5db93ab5caf64277b
Noah Gibbs <the.codefolio.guy@gmail.com>
2018-07-20 14:01:55 -0700

    Initial structure for Node.js library, no real content in place yet

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'snowfront.js',
+        library: 'snowfront',
+        libraryTarget: 'var'
+    },
+    externals: {
+        lodash: {
+            commonjs: 'lodash',
+            commonjs2: 'lodash',
+            amd: 'lodash',
+            root: '_'
+        }
+    }
+};
